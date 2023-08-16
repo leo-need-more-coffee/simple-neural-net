459fa8aab76535b9c0f30f31dae2178896b02d40
pzrnqt1vrss <pzrqt1vrss>
2023-08-16 20:59:01 +0300

    fix

--- env.py
+++ env.py
@@ -11,148 +11,155 @@ realsize = 32
 
 pix = canvas_size / realsize
 
-canvas=[[0.1 for x in range(realsize)] for y in range(realsize)]
-cellx,celly=15,5
+canvas = [[0.1 for x in range(realsize)] for y in range(realsize)]
+cellx, celly = 15, 5
 
 
 def cellvision(vis):
-    global cellx
-    global celly
-    global canvas
-    inp = []
+	global cellx
+	global celly
+	global canvas
+	inp = []
 
-    if vis != -1:
-        for j in range(vis):
-            for i in range(vis):
-                inp.append(canvas[int(cellx - vis // 2 + i) % realsize][int(celly - vis // 2 + j) % realsize])
-        return inp
-    
-    inp.append(canvas[int(cellx + 0) % realsize][int(celly - 1) % realsize])
-    inp.append(canvas[int(cellx + -1) % realsize][int(celly + 0) % realsize])
-    inp.append(canvas[int(cellx + 1) % realsize][int(celly + 0) % realsize])
-    inp.append(canvas[int(cellx + 0) % realsize][int(celly + 1) % realsize])
+	if vis != -1:
+		for j in range(vis):
+			for i in range(vis):
+				inp.append(canvas[int(cellx - vis // 2 + i) % realsize][int(celly - vis // 2 + j) % realsize])
+		return inp
 
-    return inp
+	inp.append(canvas[int(cellx + 0) % realsize][int(celly - 1) % realsize])
+	inp.append(canvas[int(cellx + -1) % realsize][int(celly + 0) % realsize])
+	inp.append(canvas[int(cellx + 1) % realsize][int(celly + 0) % realsize])
+	inp.append(canvas[int(cellx + 0) % realsize][int(celly + 1) % realsize])
+
+	return inp
 
 
 def move(out):
 	global cellx
 	global celly
-	if out==0:
-		celly-=1
-	if out==1:
-		cellx-=1
-	if out==2:
-		cellx+=1
-	if out==3:
-		celly+=1
-	if cellx==realsize:
-		cellx=0
-	if cellx==-1:
-		cellx=realsize-1
-	if celly==realsize:
-		celly=0
-	if celly==-1:
-		celly=realsize-1
-	cell(cellx,celly)
+	if out == 0:
+		celly -= 1
+	if out == 1:
+		cellx -= 1
+	if out == 2:
+		cellx += 1
+	if out == 3:
+		celly += 1
+	if cellx == realsize:
+		cellx = 0
+	if cellx == -1:
+		cellx = realsize - 1
+	if celly == realsize:
+		celly = 0
+	if celly == -1:
+		celly = realsize - 1
+	cell(cellx, celly)
 	return
 
-def goodpoint(x,y):
+
+def goodpoint(x, y):
 	color = "#476042"
-	x,y=x*pix,y*pix
-	x1, y1 = ( x - pix/2 ), ( y - pix/2 )
-	x2, y2 = ( x + pix/2 ), ( y + pix/2 )
-	w.create_oval( x1, y1, x2, y2, outline=color,fill = color )
+	x, y = x * pix, y * pix
+	x1, y1 = (x - pix / 2), (y - pix / 2)
+	x2, y2 = (x + pix / 2), (y + pix / 2)
+	w.create_oval(x1, y1, x2, y2, outline=color, fill=color)
 
-def badpoint(x,y):
+
+def badpoint(x, y):
 	color = "#ff0000"
-	x,y=x*pix,y*pix
-	x1, y1 = ( x - pix/2 ), ( y - pix/2 )
-	x2, y2 = ( x + pix/2 ), ( y + pix/2 )
-	w.create_oval( x1, y1, x2, y2, outline=color,fill = color )
+	x, y = x * pix, y * pix
+	x1, y1 = (x - pix / 2), (y - pix / 2)
+	x2, y2 = (x + pix / 2), (y + pix / 2)
+	w.create_oval(x1, y1, x2, y2, outline=color, fill=color)
 
-def cell(x,y):
+
+def cell(x, y):
 	color = "#ffffff"
-	x,y=x*pix,y*pix
-	x1, y1 = ( x - pix/2 ), ( y - pix/2 )
-	x2, y2 = ( x + pix/2 ), ( y + pix/2 )
-	w.create_oval( x1, y1, x2, y2, outline=color,fill = color )
+	x, y = x * pix, y * pix
+	x1, y1 = (x - pix / 2), (y - pix / 2)
+	x2, y2 = (x + pix / 2), (y + pix / 2)
+	w.create_oval(x1, y1, x2, y2, outline=color, fill=color)
+
 
 def canvas_print():
 	global canvas
 	w.delete("all")
-	ans=''
+	ans = ''
 	for y in range(realsize):
 		for x in range(realsize):
-			ans+=str(canvas[x][y])+" "
+			ans += str(canvas[x][y]) + " "
 			if canvas[x][y] == 1:
-				goodpoint(x,y)
+				goodpoint(x, y)
 			if canvas[x][y] == -1:
-				badpoint(x,y)
+				badpoint(x, y)
 			if canvas[x][y] == 0:
-				cell(x,y)
-		ans+="\n"
+				cell(x, y)
+		ans += "\n"
+
 
 def usergoodpoint(event):
-	x,y=int(event.x/pix),int(event.y/pix)
-	canvas[x][y]=1
+	x, y = int(event.x / pix), int(event.y / pix)
+	canvas[x][y] = 1
+
 
 def userbadpoint(event):
-	x,y=int(event.x/pix),int(event.y/pix)
-	canvas[x][y]=-1
+	x, y = int(event.x / pix), int(event.y / pix)
+	canvas[x][y] = -1
 
 
 master = Tk()
-master.title( "Среда обучения" )
+master.title("Среда обучения")
 w = Canvas(master, bg="black",
-           width=canvas_size,
-           height=canvas_size)
-w.pack(expand = YES, fill = BOTH)
-w.bind( "<B1-Motion>", usergoodpoint )
-w.bind( "<B3-Motion>", userbadpoint )
+		   width=canvas_size,
+		   height=canvas_size)
+w.pack(expand=YES, fill=BOTH)
+w.bind("<B1-Motion>", usergoodpoint)
+w.bind("<B3-Motion>", userbadpoint)
 
-iterat=-1
-allg=0
-graphic=[]
+iterat = -1
+allg = 0
+graphic = []
 while True:
-    iterat += 1
-    if iterat%200==0:
-        plt.plot(graphic)
-        plt.pause(0.0000001)
-    good=0
-    if iterat%10000==0:
-        plt.close()
-        mind.plot_weights_graph()
+	iterat += 1
+	if iterat % 200 == 0:
+		plt.plot(graphic)
+		plt.pause(0.0000001)
+	good = 0
+	if iterat % 10000 == 0:
+		plt.close()
+		mind.plot_weights_graph()
 
+	canvas[random.randint(0, realsize - 1)][random.randint(0, realsize - 1)] = 1
+	canvas[random.randint(0, realsize - 1)][random.randint(0, realsize - 1)] = -1
 
-    canvas[random.randint(0,realsize-1)][random.randint(0,realsize-1)] = 1
-    canvas[random.randint(0,realsize-1)][random.randint(0,realsize-1)] = -1
+	canvas_print()
 
-    canvas_print()
-	
-    visn=cellvision(5)
-    visnn=cellvision(-1)
+	visn = cellvision(5)
+	visnn = cellvision(-1)
 
-    out, error = mind.correct(visn, visnn)
-    print(out)
-    move_ = out[-1].index(max(out[-1]))
-    move(move_)
+	out = mind.out(visn)
+	move_ = out[-1].index(max(out[-1]))
+	mind.correct(visn, visnn, 0.1)
 
-    if canvas[cellx][celly]==1:
-        good+=50
-        canvas[cellx][celly]=0.1
+	answer = [0]*4
+	answer[move_] = 1
+	move(move_)
 
-    elif canvas[cellx][celly]==-1:
-        good-=50
-        canvas[cellx][celly]=0.1
+	if canvas[cellx][celly] == 1:
+		good += 50
+		canvas[cellx][celly] = 0.1
 
+	elif canvas[cellx][celly] == -1:
+		good -= 50
+		canvas[cellx][celly] = 0.1
 
-	#print(input())
-    allg+=good
-    graphic.append(allg)
+	# print(input())
+	allg += good
+	graphic.append(allg)
 
-    master.title( "Среда обучения: "+" i:"+ str(iterat)+" good:"+str(good))
-    master.update()
+	master.title("Среда обучения: " + " i:" + str(iterat) + " good:" + str(good))
+	master.update()
 
 plt.show()
 master.mainloop()
--- neuro.py
+++ neuro.py
@@ -1,46 +1,51 @@
 import math
 import random
-import numba
-import matplotlib.pyplot as plt
-import networkx as nx
-from networkx.drawing.nx_agraph import graphviz_layout
+import time
+import datetime
+import json
+from copy import copy
 
 activation = math.tanh
+deactivation = math.tanh
+
 
 class NeuralNetwork:
-    def __init__(self, neurons_size:list, weights:list=None):
+    def __init__(self, neurons_size: list, weights: list = None):
         self.neuron_size = neurons_size
+        for i in range(len(neurons_size) - 1):
+            self.neuron_size[i] += 1
+
         if weights is None:
             self.weights = []
-            for i in range(len(neurons_size)-1):
-                self.weights.append([[random.uniform(-1, 1) for x in range(neurons_size[i+1])] for y in range(neurons_size[i])])
+            for i in range(len(neurons_size) - 1):
+                self.weights.append(
+                    [[random.uniform(-1, 1) for x in range(neurons_size[i + 1])] for y in range(neurons_size[i])])
         else:
             self.weights = weights
-        
+
     def out(self, inp):
-        out=[inp]
-        for i in range(1, len(self.weights)+1):
+        out = [inp + [1]]
+        for i in range(1, len(self.weights) + 1):
             a = []
             for j in range(self.neuron_size[i]):
-                s = sum([out[i-1][k] * self.weights[i-1][k][j] for k in range(self.neuron_size[i-1])])
+                s = sum([out[i - 1][k] * self.weights[i - 1][k][j] for k in range(self.neuron_size[i - 1])])
                 a.append(activation(s))
+            if i != len(self.weights):
+                a += [1]
             out.append(a)
         return out
-            
+
     def correct(self, inp, answer, learning_rate=0.1):
         out = self.out(inp)
         errors = [[answer[i] - out[-1][i] for i in range(len(out[-1]))]]
 
-        # Backpropagate the errors and update weights
         for i in range(len(self.weights) - 1, 0, -1):
             a = []
             for j in range(self.neuron_size[i]):
                 s = sum([errors[0][k] * self.weights[i][j][k] for k in range(self.neuron_size[i + 1])])
-                a.append((1 - out[i][j] ** 2) * s)  # Corrected error using derivative of tanh activation
+                a.append((1 - out[i][j] ** 2) * s)
             errors.insert(0, a)
 
-
-        # Update the weights using the computed errors
         for i in range(len(self.weights)):
             for j in range(self.neuron_size[i]):
                 for k in range(self.neuron_size[i + 1]):
@@ -49,15 +54,46 @@ class NeuralNetwork:
         error_count = sum([sum(abs(en) for en in el) for el in errors])
         return out, error_count
 
-    def plot_weights_graph(self):
+    def mutate(self, mutate_k):
+        for layer in range(len(self.weights)):
+            for inp in range(len(self.weights[layer])):
+                for outp in range(len(self.weights[layer][inp])):
+                    self.weights[layer][inp][outp] += random.uniform(-mutate_k, mutate_k)
+
+    def difference(self, other):
+        if self.neuron_size != other.neuron_size:
+            return None
+        difference_count = 0
+        for layer in range(len(self.weights)):
+            for inp in range(len(self.weights[layer])):
+                for outp in range(len(self.weights[layer][inp])):
+                    difference_count += abs(self.weights[layer][inp][outp] - other.weights[layer][inp][outp])
+
+        return difference_count
+
+    def save(self, name):
+        with open(name, 'w') as f:
+            neuron_size = copy(self.neuron_size)
+            for i in range(len(neuron_size) - 1):
+                neuron_size[i] -= 1
+            f.write(json.dumps({'shape': neuron_size, 'weights': self.weights}))
+
+    def open(name):
+        with open(name, 'r') as f:
+            data = json.loads(f.read())
+            return NeuralNetwork(data['shape'], data['weights'])
+
+    def show(self, name):
+        import matplotlib.pyplot as plt
+        import networkx as nx
+        from networkx.drawing.nx_agraph import graphviz_layout
+
         G = nx.DiGraph()
 
-        # Add nodes to the graph for each layer
         for layer in range(len(self.neuron_size)):
             for neuron in range(self.neuron_size[layer]):
                 G.add_node((layer, neuron))
 
-        # Add edges to the graph for weights between neurons
         for layer in range(len(self.neuron_size) - 1):
             for from_neuron in range(self.neuron_size[layer]):
                 for to_neuron in range(self.neuron_size[layer + 1]):
@@ -67,25 +103,19 @@ class NeuralNetwork:
         pos = graphviz_layout(G, prog='dot', args="-Grankdir=LR")
 
         edge_widths = [2 + abs(G.edges[edge]['weight']) for edge in G.edges]
-        edge_alpha = [abs(activation(G.edges[edge]['weight']))/2 for edge in G.edges]
+        edge_alpha = [abs(activation(G.edges[edge]['weight'])) / 2 for edge in G.edges]
         edge_colors = ['green' if G.edges[edge]['weight'] >= 0 else 'red' for edge in G.edges]
-        # Draw nodes
 
         nx.draw_networkx_nodes(G, pos, node_size=300, node_color='skyblue', alpha=0.8)
 
-        # Draw edges
-        #edge_labels = {(edge[0], edge[1]): G.edges[edge]['weight'] for edge in G.edges}
         nx.draw_networkx_edges(G, pos, width=edge_widths, alpha=edge_alpha, edge_color=edge_colors, arrows=True)
-        # Draw edge labels (weights)
-        #nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=10)
 
-        # Draw layer labels
         layer_labels = {}
         for layer in range(len(self.neuron_size)):
             for neuron in range(self.neuron_size[layer]):
-                    layer_labels[(layer, neuron)] = f"{neuron}"
+                layer_labels[(layer, neuron)] = f"{neuron}"
         nx.draw_networkx_labels(G, pos, labels=layer_labels, font_size=12, font_color='r')
 
         plt.axis('off')
-        plt.title("Neural Network Weights")
-        plt.show()
+        plt.title(f"Neural Network Weights")
+        plt.savefig(f'{name}.png')
